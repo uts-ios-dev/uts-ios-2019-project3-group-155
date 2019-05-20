071d0dca1d9e2357485d072bd61e6fc1a5fd51a9
Aruna Sairam <arunsairam.maverick@gmail.com>
2019-05-21 00:09:03 +1000

    Addition of Lables

--- Money Matter/ViewController.swift
+++ Money Matter/ViewController.swift
@@ -19,49 +19,89 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     @IBOutlet weak var currencyPicker3: UIPickerView!
     
-    @IBOutlet weak var currencyTrigger1: UITextField!
+    
+    @IBOutlet weak var done1: UIButton!
+    
+    @IBOutlet weak var done2: UIButton!
+    
+    @IBOutlet weak var done3: UIButton!
+    
     @IBOutlet weak var currencyButton1: UIButton!
     @IBOutlet weak var currencyLabel1: UILabel!
     
-    @IBOutlet weak var currencyTrigger2: UITextField!
     @IBOutlet weak var currencyButton2: UIButton!
     @IBOutlet weak var currencyLabel2: UILabel!
-    
-    @IBOutlet weak var currencyTrigger3: UITextField!
+
     @IBOutlet weak var currencyButton3: UIButton!
     @IBOutlet weak var currencyLabel3: UILabel!
     
+  
+    @IBOutlet weak var stackView1: UIStackView!
+    
+    @IBOutlet weak var stackView2: UIStackView!
+    
+    @IBOutlet weak var stackView3: UIStackView!
+    
     @IBAction func button1Pressed(_ sender: Any) {
         currencyPicker1.isHidden=false
+        currencyLabel1.isHidden = true
+        done1.isHidden=false
+        stackView1.isHidden=true
     }
     
     @IBAction func button2Pressed(_ sender: Any) {
         currencyPicker2.isHidden=false
+        currencyLabel2.isHidden = true
+        done2.isHidden=false
+        stackView2.isHidden=true
     }
     
     @IBAction func button3Pressed(_ sender: Any) {
         currencyPicker3.isHidden=false
+        currencyLabel3.isHidden = true
+        done3.isHidden=false
+        stackView3.isHidden=true
+    }
+    
+    @IBAction func done1Pressed(_ sender: Any) {
+        currencyPicker1.isHidden = true
+        done1.isHidden=true
+        currencyLabel1.isHidden = false
+        stackView1.isHidden=false
     }
     
     
-    
-    var data: [(currency: String, image: UIImage?)] = [("Select Currency",UIImage(named: "add.gif")),("INR",UIImage(named: "india.png")),("AUD",UIImage(named: "aus.png")),("USD",UIImage(named:"usa.png"))]
-    var currencyPicker = UIPickerView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        currencyPicker1.isHidden=true
-        currencyPicker2.isHidden=true
-        currencyPicker3.isHidden=true
-        /*  currencyPicker.delegate=self
-         currencyPicker.dataSource=self
-         currencyTrigger1.inputView=currencyPicker
-         currencyTrigger2.inputView=currencyPicker
-         currencyTrigger3.inputView=currencyPicker*/
+    @IBAction func done2Pressed(_ sender: Any) {
+        currencyPicker2.isHidden = true
+        done2.isHidden=true
+        currencyLabel2.isHidden = false
+        stackView2.isHidden=false
         
     }
     
+    @IBAction func done3Pressed(_ sender: Any) {
+        currencyPicker3.isHidden = true
+        done3.isHidden=true
+        currencyLabel3.isHidden = false
+        stackView3.isHidden=false
+        
+    }
+    var data: [(currency: String, image: UIImage?)] = [("Select Currency",UIImage(named: "add.gif")),("INR",UIImage(named: "india.png")),("AUD",UIImage(named: "aus.png")),("USD",UIImage(named:"usa.png"))]
+    var currencyPicker = UIPickerView()
+   
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        done1.isHidden=true
+        done2.isHidden=true
+        done3.isHidden=true
+        currencyPicker1.isHidden=true
+        currencyPicker2.isHidden=true
+        currencyPicker3.isHidden=true
+        
+    }
+    
+
     
     public func numberOfComponents(in pickerView: UIPickerView) -> Int{
         return 1
@@ -76,17 +116,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         if pickerView.tag == 1{
             currencyLabel1.text=data[row].currency
             currencyButton1.setImage(data[row].image!,for: .normal)
-            pickerView.isHidden = true
+        
+            
         }
         else if pickerView.tag == 2{
             currencyLabel2.text=data[row].currency
             currencyButton2.setImage(data[row].image!,for: .normal)
-            pickerView.isHidden = true
+          
         }
         else{
             currencyLabel3.text=data[row].currency
             currencyButton3.setImage(data[row].image!,for: .normal)
-            pickerView.isHidden = true
+           
         }
         
     }
@@ -94,6 +135,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return data[row].currency
         
     }
+    
+    
     
 }
 
